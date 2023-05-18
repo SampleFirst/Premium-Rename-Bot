b84ae1c3d7db46af0d30508e7aef73f6c880e2e7
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-05-19 00:42:20 +0530

    Update progress.py

--- helper/progress.py
+++ helper/progress.py
@@ -64,13 +64,13 @@ def time_formatter(milliseconds: int) -> str:
     days, hours = divmod(hours, 24)
     tmp = (
         (str(days) + "d, ") if days else ""
-    ) + (
-        (str(hours) + "h, ") if hours else ""
-    ) + (
-        (str(minutes) + "m, ") if minutes else ""
-    ) + (
-        (str(seconds) + "s, ") if seconds else ""
-    ) + (
-        (str(milliseconds) + "ms, ") if milliseconds else ""
+    ) + \
+        ((str(hours) + "h, ") if hours else ""
+    ) + \
+        ((str(minutes) + "m, ") if minutes else ""
+    ) + \
+        ((str(seconds) + "s, ") if seconds else ""
+    ) + \
+        ((str(milliseconds) + "ms, ") if milliseconds else ""
     )
     return tmp[:-2]
