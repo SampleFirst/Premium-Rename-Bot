42baf7e92ef4867053da6d46ab3986e359dc6d6d
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-05-19 01:26:43 +0530

    Update refer.py

--- plugins/refer.py
+++ plugins/refer.py
@@ -1,13 +1,9 @@
 from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ForceReply
+from pyrogram.types import (InlineKeyboardButton, InlineKeyboardMarkup, ForceReply)
 
 @Client.on_message(filters.private & filters.command(["refer"]))
 async def refer(client, message):
     reply_markup = InlineKeyboardMarkup(
         [[InlineKeyboardButton("Share Your Link", url=f"https://t.me/share/url?url=https://t.me/PremiumRenameBot?start={message.from_user.id}")]]
     )
-    await message.reply_text(
-        "Refer And Earn Get 250MB Upload Limit\nPer Refer 250MB",
-        reply_to_message_id=message.message_id,
-        reply_markup=reply_markup,
-    )
+    await message.reply_text(f"Refer And Earn Get 250MB Upload Limit\nPer Refer 250MB\n Your Link :- https://t.me/PremiumRenameBot?start={message.from_user.id} ",reply_to_message_id = message.id,reply_markup=reply_markup,)
