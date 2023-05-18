cc644b6b5bc8c5d6e73d91b698dc5e6c19344465
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-05-19 01:20:03 +0530

    Update myplane.py

--- plugins/myplane.py
+++ plugins/myplane.py
@@ -1,20 +1,25 @@
 import time
 from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from helper.database import (
-    find_one, used_limit, daily as daily_, uploadlimit, usertype
-)
+from pyrogram.types import (
+    InlineKeyboardButton, InlineKeyboardMarkup, ForceReply)
+from helper.database import find_one, used_limit
+from helper.database import daily as daily_
+import datetime
+from datetime import timedelta, date, datetime
+from datetime import date as date_
 from helper.progress import humanbytes
 from helper.date import check_expi
-from datetime import timedelta, date, datetime
+from helper.database import uploadlimit, usertype
+
 
 @Client.on_message(filters.private & filters.command(["myplan"]))
 async def start(client, message):
     used_ = find_one(message.from_user.id)
     daily = used_["daily"]
-    expi = daily - int(time.mktime(time.strptime(str(date.today()), '%Y-%m-%d')))
+    expi = daily - \
+        int(time.mktime(time.strptime(str(date_.today()), '%Y-%m-%d')))
     if expi != 0:
-        today = date.today()
+        today = date_.today()
         pattern = '%Y-%m-%d'
         epcho = int(time.mktime(time.strptime(str(today), pattern)))
         daily_(message.from_user.id, epcho)
@@ -27,16 +32,16 @@ async def start(client, message):
     ends = _newus["prexdate"]
     if ends:
         pre_check = check_expi(ends)
-        if not pre_check:
+        if pre_check == False:
             uploadlimit(message.from_user.id, 1288490188)
             usertype(message.from_user.id, "Free")
-    if ends is None:
-        text = f"User ID: ```{message.from_user.id}```\nPlan: {user}\nDaily Upload Limit: {humanbytes(limit)}\nToday Used: {humanbytes(used)}\nRemain: {humanbytes(remain)}"
+    if ends == None:
+        text = f"User ID:- ```{message.from_user.id}```\nPlan :- {user}\nDaly Upload Limit :- {humanbytes(limit)}\nToday Used :- {humanbytes(used)}\nRemain:- {humanbytes(remain)}"
     else:
         normal_date = datetime.fromtimestamp(ends).strftime('%Y-%m-%d')
-        text = f"User ID: ```{message.from_user.id}```\nPlan: {user}\nDaily Upload Limit: {humanbytes(limit)}\nToday Used: {humanbytes(used)}\nRemain: {humanbytes(remain)}\n\nYour Plan Ends On: {normal_date}"
+        text = f"User ID:- ```{message.from_user.id}```\nPlan :- {user}\nDaly Upload Limit :- {humanbytes(limit)}\nToday Used :- {humanbytes(used)}\nRemain:- {humanbytes(remain)}\n\nYour Plan Ends On :- {normal_date}"
 
     if user == "Free":
-        await message.reply(text, quote=True, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("💰 Upgrade", callback_data="upgrade"), InlineKeyboardButton("Cancel ✖️", callback_data="cancel")]]))
+        await message.reply(text, quote=True, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Upgrade 💰💳", callback_data="upgrade"), InlineKeyboardButton("Cancel ✖️ ", callback_data="cancel")]]))
     else:
         await message.reply(text, quote=True)
