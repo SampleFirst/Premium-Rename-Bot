80d829c22bc4b5e6c1462996342119808b905010
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-05-18 15:03:04 +0530

    Update database.py

--- helper/database.py
+++ helper/database.py
@@ -9,10 +9,14 @@ mongo = pymongo.MongoClient(DB_URL)
 db = mongo[DB_NAME]
 dbcol = db["user"]
 
-# total users
+
+# Total User
+
 def total_user():
     user = dbcol.count_documents({})
     return user
+
+# insert bot Data
 
 def botdata(chat_id):
     bot_id = int(chat_id)
@@ -22,14 +26,18 @@ def botdata(chat_id):
     except:
         pass
 
+
 def total_rename(chat_id, renamed_file):
     now = int(renamed_file) + 1
     dbcol.update_one({"_id": chat_id}, {"$set": {"total_rename": str(now)}})
+
 
 def total_size(chat_id, total_size, now_file_size):
     now = int(total_size) + now_file_size
     dbcol.update_one({"_id": chat_id}, {"$set": {"total_size": str(now)}})
 
+
+# insert user data
 def insert(chat_id):
     user_id = int(chat_id)
     user_det = {"_id": user_id, "file_id": None, "caption": None, "daily": 0, "date": 0,
@@ -40,39 +48,51 @@ def insert(chat_id):
         return True
         pass
 
+
 def addthumb(chat_id, file_id):
     dbcol.update_one({"_id": chat_id}, {"$set": {"file_id": file_id}})
+
 
 def delthumb(chat_id):
     dbcol.update_one({"_id": chat_id}, {"$set": {"file_id": None}})
 
+
 def addcaption(chat_id, caption):
     dbcol.update_one({"_id": chat_id}, {"$set": {"caption": caption}})
+
 
 def delcaption(chat_id):
     dbcol.update_one({"_id": chat_id}, {"$set": {"caption": None}})
 
+
 def dateupdate(chat_id, date):
     dbcol.update_one({"_id": chat_id}, {"$set": {"date": date}})
+
 
 def used_limit(chat_id, used):
     dbcol.update_one({"_id": chat_id}, {"$set": {"used_limit": used}})
 
+
 def usertype(chat_id, type):
     dbcol.update_one({"_id": chat_id}, {"$set": {"usertype": type}})
 
+
 def uploadlimit(chat_id, limit):
     dbcol.update_one({"_id": chat_id}, {"$set": {"uploadlimit": limit}})
+
 
 def addpre(chat_id):
     date = add_date()
     dbcol.update_one({"_id": chat_id}, {"$set": {"prexdate": date[0]}})
 
+
 def addpredata(chat_id):
     dbcol.update_one({"_id": chat_id}, {"$set": {"prexdate": None}})
 
+
 def daily(chat_id, date):
     dbcol.update_one({"_id": chat_id}, {"$set": {"daily": date}})
+
 
 def find(chat_id):
     id = {"_id": chat_id}
@@ -86,13 +106,17 @@ def find(chat_id):
 
         return [file, caption]
 
+
 def getid():
     values = []
     for key in dbcol.find():
         id = key["_id"]
-        values.append(id)
+        values.append((id))
     return values
 
 def delete(id):
     dbcol.delete_one(id)
 
+
+def find_one(id):
+    return dbcol.find_one({"_id": id})
