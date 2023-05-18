0822c48bae878977a715c7850632ea238bed7ab9
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-05-19 01:04:39 +0530

    Update caption.py

--- plugins/caption.py
+++ plugins/caption.py
@@ -5,24 +5,25 @@ from helper.database import *
 @Client.on_message(filters.private & filters.command('set_caption'))
 async def add_caption(client, message):
     if len(message.command) == 1:
-        return await message.reply_text("**Give me a caption to set.\n\nExample: `/set_caption File Name`**")
+       return await message.reply_text("**Give me a caption to set.\n\nExample:- `/set_caption File Name`**")
     caption = message.text.split(" ", 1)[1]
-    add_caption(int(message.chat.id), caption)
+    addcaption(int(message.chat.id), caption)
     await message.reply_text("**Your caption has been successfully added ✅**")
 
 @Client.on_message(filters.private & filters.command('del_caption'))
-async def delete_caption(client, message):
-    caption = find_caption(int(message.chat.id))
+async def delete_caption(client, message): 
+    caption = find(int(message.chat.id))[1]
     if not caption:
-        await message.reply_text("**You don't have any custom caption**")
+        await message.reply_text("**You dont have any custom caption**")
         return
-    delete_caption(int(message.chat.id))
+    delcaption(int(message.chat.id))
     await message.reply_text("**Your caption has been successfully deleted ✅**")
-
+    
 @Client.on_message(filters.private & filters.command('see_caption'))
-async def see_caption(client, message):
-    caption = find_caption(int(message.chat.id))
+async def see_caption(client, message): 
+    caption = find(int(message.chat.id))[1]
     if caption:
-        await message.reply_text(f"<b><u>Your Caption:</u></b>\n\n`{caption}`", parse_mode="HTML")
+       await message.reply_text(f"<b><u>Your Caption:</b></u>\n\n`{caption}`")
     else:
-        await message.reply_text("**You don't have any custom caption**")
+       await message.reply_text("**You dont have any custom caption**")
+          
