a16d0914c07e1b53f1a09a653c871551bebe564b
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-05-19 19:35:18 +0530

    Update admin.py

--- plugins/admin.py
+++ plugins/admin.py
@@ -4,7 +4,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from helper.date import add_date
 from helper.database import uploadlimit, usertype, addpre
 
-ADMIN = int(os.environ.get("ADMIN", ""))
+ADMIN = int(os.environ.get("ADMIN", "5217294686"))
 log_channel = int(os.environ.get("LOG_CHANNEL", ""))
 
 @Client.on_message(filters.private & filters.user(ADMIN) & filters.command(["warn"]))
