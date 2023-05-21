336d02aca69d31e796f043623f6e08d78f38dc47
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-05-21 21:50:19 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ from plugins.cb_data import app as Client2
 TOKEN = os.environ.get("TOKEN", "5873664112:AAEMhsbdC4TCLp_efpJXw9eWeqg1dmREdaM")
 API_ID = int(os.environ.get("API_ID", "23906038"))
 API_HASH = os.environ.get("API_HASH", "dff1eb42fad7971f16da662a99c0f376")
-STRING = os.environ.get("STRING", "BQFsxvYAHVTc1i9p2Ho-RUhkmImuffvebIp5mapamUPnaN7g_Gs2dcveQ9Wbn2nj8kmc2G9Tp6aIYPsr33PG-Wl-XaeCqgdofZCibuoHfFT5afkZ8AkG7Vm5fukWYdJZi-vtr40E95nFwGLqw46zB2oZ1pwC78-j-b1YZT46qfkRliY0Mhp6PysJhz2BzF0wzBEhYQ3kb3COndNRgdbDooZKDBekFaOPRf-VdNXDGkyUzzZKM029x2YNLDGaWYlzsI_sabZRsETmjxu-604mCnl05SgI1nb9Kynl9GbjRtGxxoXTKWqiuy45NUzEd2-tNt3Ht-rT6EaPsDRXgpkaB4ApBv4A9wAAAAE2-ZleAA")
+STRING = os.environ.get("STRING", "")
 
 
 def update_session_string(session_string):
