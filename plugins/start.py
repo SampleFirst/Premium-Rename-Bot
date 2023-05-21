d6de95073acf159751c89ea7328151ac45dded5f
Sample First <132287607+SampleFirst@users.noreply.github.com>
2023-05-21 20:07:47 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -42,7 +42,7 @@ async def start(client, message):
     try:
         id = message.text.split(' ')[1]
     except:
-        txt=f"""Hello {wish} {message.from_user.first_name }\n\nI am file renamer bot, Please sent any telegram**Document Or Video** and enter new filename to rename it"""
+        txt=f"""Hello {wish} {message.from_user.first_name }\n\nI am file renamer bot, Please sent any telegram **Document Or Video** and enter new filename to rename it"""
         await message.reply_photo(photo=LAZY_PIC, caption=txt, reply_markup=InlineKeyboardMarkup([
             [InlineKeyboardButton("🤖 ᴍᴏʀᴇ ʙᴏᴛs 🤖", callback_data='bots')],
             [InlineKeyboardButton('🔔 ᴜᴘᴅᴀᴛᴇ', url='https://t.me/iPepkornBots'),
